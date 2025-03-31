c5831da9753082df40d1fbad79671515f06d4d16
Challa Ravindranath <chravin@microsoft.com>
2025-03-31 08:17:46 +0530

    Auth Paackages and basic Set up

---
+++ ProductApi/ProductApi/Auth/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ProductApi.Auth
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string RefreshToken { get; set; }
+        public DateTime? RefreshToikenExpiryTime { get; set; }
+    }
+}
